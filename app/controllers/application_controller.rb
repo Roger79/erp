9ec20ceef6f51e82ccde72db498e4e83e125f395
Looser <looser.roger@gmail.com>
2018-05-23 20:19:36 +0200

    Hello world
    View set up for trial

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,2 +1,12 @@
 class ApplicationController < ActionController::Base
+
+  def hello
+    render html: "hello world"
+  end
+
+  def sign
+    render html: "Fig die Henne du schwuchtel"
+  end
+
+
 end
